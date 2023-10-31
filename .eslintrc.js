f41c6cc55140c6402e89ef417be3117bfe8e7b65
DiogoBlanco <diogoblanco@outlook.com.br>
2023-10-31 20:07:36 -0300

    initial

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {},
+};
--- src/templates/Home/Home.jsx
+++ src/templates/Home/Home.jsx
@@ -1,4 +1,5 @@
-import "./Home.css";
+import React from 'react';
+import './Home.css';
 
 function Home() {
   return <h1>Home</h1>;
